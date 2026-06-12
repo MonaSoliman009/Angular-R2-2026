3f31bff1767389d45cf5ab4894ba756f481d9ea6
MonaSoliman009 <monasoliman009@gmail.com>
2026-06-12 12:17:55 +0300

    feat(Day 1):intro & interpolation & property binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,22 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Home } from './components/home/home';
+import { Navbar } from './components/navbar/navbar';
+import { Footer } from './components/footer/footer';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Home,Navbar,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
+  // template:`
+  // <h1>Hello from ts file</h1>
+  // `,
+  // styles:[
+  //   `h1{
+  //     color: red;
+  //   }`
+  // ]
 })
 export class App {
   protected readonly title = signal('lec1');
