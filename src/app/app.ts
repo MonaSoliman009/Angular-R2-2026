0348ed7bcc22f32fc5deb0e228ac8272eb29421e
MonaSoliman009 <monasoliman009@gmail.com>
2026-06-13 12:19:54 +0300

    feat(Day 2): Data binding & Built-in directives

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { Home } from './components/home/home';
 import { Navbar } from './components/navbar/navbar';
 import { Footer } from './components/footer/footer';
+import { ProductsList } from './components/products-list/products-list';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Home,Navbar,Footer],
+  imports: [ProductsList,Navbar,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
   // template:`
--- src/app/models/iproduct.ts
+++ src/app/models/iproduct.ts
@@ -1,6 +1,8 @@
 export interface IProduct {
-  name: string,
-  price: number,
-  description: string,
-  imgUrl: string
+  id:number,
+  name:string,
+  price:number,
+  quantity:number,
+  imgUrl:string,
+  catId:number
 }
